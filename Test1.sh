778a70db368a22c0d8992c560c166be176b9d320
GanesanPsy <108250659+GanesanPsy@users.noreply.github.com>
2022-06-29 00:38:22 +0800

    Update Test1.sh

--- Test1.sh
+++ Test1.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "test1"
-param=$(yq '.variables.param' azure-pipelines.yml)
-echo " test2" $(param)
+param=$(yq '.variables.Test' azure-pipelines.yml)
+echo " test2" $param
 function main(){
     echo
     echo
