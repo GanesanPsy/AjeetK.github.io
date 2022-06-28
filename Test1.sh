a444ff4c1514c68b470060e7aa5ffc46617a1075
GanesanPsy <108250659+GanesanPsy@users.noreply.github.com>
2022-06-28 12:45:36 +0800

    Update Test1.sh

--- Test1.sh
+++ Test1.sh
@@ -1,2 +1,18 @@
 #!/bin/bash
 echo "Hello World"
+function main(){
+    echo
+    echo
+    echo "                   POC"
+    echo
+    echo "---------------------------------------------------------------------"
+    echo "This script sets up an evaluation version "
+    echo "---------------------------------------------------------------------"
+    echo
+    echo " Please ensure the following are set up before proceeding."
+    echo " 1. Install Docker & Docker Compose"
+    echo " 2. Install Git & obtain GitHub Personal Access Token"
+    echo 
+    read -p " Proceed? (y/n) " -n 1 -r
+    echo
+    }
