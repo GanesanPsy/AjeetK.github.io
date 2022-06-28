f6a9ed54c42d7faf28aa2c78fe0a9d11ebeb2d79
GanesanPsy <108250659+GanesanPsy@users.noreply.github.com>
2022-06-28 12:06:54 +0800

    Update Test.sh

--- Test.sh
+++ Test.sh
@@ -1,6 +1,53 @@
 #!/bin/bash
 
-# example of using arguments to a script
-echo "My first name is G"
-echo "My surname is P"
-echo "Total number of arguments is $#" 
+# Timestamp Function
+timestamp() {
+    date +"%T"
+}
+
+# Temporary file for stderr redirects
+tmpfile=$(mktemp)
+
+# Go build
+build () {
+    echo "    $(timestamp): started build script..."
+    echo "   $(timestamp): building cicdexample"
+    go build 2>tmpfile
+    if [ -s tmpfile ]; then
+        cat tmpfile
+        echo "   $(timestamp): compilation error, exiting"
+        rm tmpfile
+        exit 0
+    fi
+}
+
+# Deploy to Minikube using kubectl
+deploy() {
+    echo "  $(timestamp): deploying to Minikube"
+    kubectl apply -f deploy.yml
+}
+
+# Orchestrate
+echo " Welcome to the Builder v0.2, written by github.com/cishiv"
+if [[ $1 = "build" ]]; then
+    if [[ $2 = "docker" ]]; then
+        build
+        buildDocker
+        echo "    $(timestamp): complete."
+        echo " $(timestamp): exiting..."
+    elif [[ $2 = "bin" ]]; then
+        build
+        echo "   $(timestamp): complete."
+        echo "  $(timestamp): exiting..."
+    else
+        echo "   $(timestamp): missing build argument"
+    fi
+else
+    if [[ $1 = "--help" ]]; then
+        echo "build - start a build to produce artifacts"
+        echo "  docker - produces docker images"
+        echo "  bin - produces executable binaries"
+    else
+        echo "$(timestamp): no arguments passed, type --help for a list of arguments"
+    fi
+fi
