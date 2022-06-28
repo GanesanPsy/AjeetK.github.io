5d41e77f3ac3a708c9b1324146b2865291e9ff6d
GanesanPsy <108250659+GanesanPsy@users.noreply.github.com>
2022-06-28 22:03:56 +0800

    Update Test1.sh

--- Test1.sh
+++ Test1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "Hello World"
+
 function main(){
     echo
     echo
@@ -16,3 +16,51 @@ function main(){
 
     echo
     }
+
+# Timestamp Function
+timestamp() {
+    date +"%T" 
+}
+
+# Temporary file for stderr redirects
+
+# Go build
+build () {
+    echo "    $(timestamp): started build script..."
+    echo "   $(timestamp): building cicdexample"
+        echo "   $(timestamp): compilation error, exiting"
+        exit 0
+}
+
+# Deploy to Minikube using kubectl
+deploy() {
+    echo "  $(timestamp): deploying to Minikube"
+    kubectl apply -f deploy.yml
+}
+
+# Orchestrate
+echo " Welcome to the Pilot Project!" $Param
+main
+if [[ $1 = "build" ]]; then
+echo " Welcome to the Pilot Project!"
+    if [[ $2 = "docker" ]]; then
+        build
+        buildDocker
+        echo "    $(timestamp): complete."
+        echo " $(timestamp): exiting..."
+    elif [[ $2 = "bin" ]]; then
+        build
+        echo "   $(timestamp): complete."
+        echo "  $(timestamp): exiting..."
+    else
+        echo "   $(timestamp): missing build argument"
+    fi
+else
+    if [[ $1 = "--help" ]]; then
+        echo "build - start a build to produce artifacts"
+        echo "  docker - produces docker images"
+        echo "  bin - produces executable binaries"
+    else
+        echo "$(timestamp): no arguments passed, type --help for a list of arguments"
+    fi
+fi
